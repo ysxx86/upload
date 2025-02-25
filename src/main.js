fd109f5af166313b64f25673ed93dac87d4d6e71
kmduyy <492610718@qq.com>
2025-02-25 08:53:03 +0800

    2

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(ElementPlus)
-
-// 注册所有图标
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
 app.mount('#app')
